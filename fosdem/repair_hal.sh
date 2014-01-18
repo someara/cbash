49f557b1d7b98d8d2e8a52435b922a66bb5b5166
Sean OMeara <someara@opscode.com>
2014-01-18 00:52:50 -0500

    savegame

--- fosdem/repair_hal.sh
+++ fosdem/repair_hal.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 source ../lib/functions.sh
 
-group space 2001
-user hal 9000 2001 'My mind is going. I can feel it.' /home/hal /bin/bash
-directory /home/hal/files hal 2001 0755
-update_file /mnt/dist/hal/the_pod_doors /home/hal/files/
+group space \
+    2001
+
+user hal \
+    9000 \
+    2001 \
+    'My mind is going. I can feel it.' \
+    /home/hal \
+    /bin/bash
+
+directory /home/hal/files \
+    hal \
+    2001 \
+    0755
+
+update_file /mnt/dist/hal/the_pod_doors \
+    /home/hal/files/
+
+
 
